7bf8aecc3f012198e86a19f03c8e50a0d521ad18
Ryut4919 <tzhw.lai@gmail.com>
2018-01-26 04:41:55 +0900

    Boss2
    Add Normal(LeftHand)

--- Samurai_No_Ibuki/Assets/script/Boss2Controller.cs
+++ Samurai_No_Ibuki/Assets/script/Boss2Controller.cs
@@ -9,8 +9,7 @@ public class Boss2Controller : MonoBehaviour {
         Idle=0,
         NormalAttack=1,
         SwordAttack=2,
-        TwoBreak=3,
-        Dead=4,
+        Dead=3,
     };
 
     public BossState BState;
@@ -18,6 +17,7 @@ public class Boss2Controller : MonoBehaviour {
     public GameObject Body;
     public GameObject LeftHand;
     public GameObject RightHand;
+    public GameObject Player;
 
     public float Blood_Body;
     public float Blood_LeftHand;
@@ -25,8 +25,11 @@ public class Boss2Controller : MonoBehaviour {
     public float Def_Buff;
     public float Skill_CD_Time;
     public float AttackSpeed;
+    public float AttackTime;
+    public float AttackColdDown;
 
-
+    public bool NormalAttack;
+    public bool SpAtk;
 
     // Use this for initialization
     void Start () {
@@ -34,11 +37,74 @@ public class Boss2Controller : MonoBehaviour {
         Blood_LeftHand = 100.0f;
         Blood_RightHand = 100.0f;
         Def_Buff = 3.0f;
-
+        AttackTime = 5.0f;
+        AttackColdDown = 5.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        normalAttack();
+        LookPlayer();
+        if (NormalAttack)
+        {
+            if (this.transform.localScale == new Vector3(1, 1, 1))
+            {
+                LeftHand.transform.Translate(-0.3f, 0, 0);
+                if (LeftHand.transform.position.x <= Player.transform.position.x)
+                {
+                    LeftHand.transform.position = new Vector3(Player.transform.position.x-2, LeftHand.transform.position.y, 0);
+                    StartCoroutine("AtkDown");
+                }
+            }
+           
+            else if (this.transform.localScale == new Vector3(-1, 1, 1))
+            {
+                LeftHand.transform.Translate(-0.3f, 0, 0);
+                if (LeftHand.transform.position.x >= Player.transform.position.x)
+                {
+                    LeftHand.transform.position = new Vector3(Player.transform.position.x + 2, LeftHand.transform.position.y, 0);
+                    StartCoroutine("AtkDown");
+                }
+            }
+            AttackTime -= Time.deltaTime;
+        }
+        if (AttackTime <= 0)
+        {
+            NormalAttack = false;
+            AttackColdDown -= Time.deltaTime;
+            if (AttackColdDown <= 0)
+            {
+                AttackTime = 5.0f;
+                AttackColdDown = 5.0f;
+            }
+        }
 	}
+
+    private void normalAttack()
+    {
+        if (!NormalAttack && SpAtk == false && AttackColdDown >= 5.0f)
+        {
+            NormalAttack = true;
+        }
+    }
+
+    private void LookPlayer()
+    {
+        if (Player.transform.position.x > this.transform.position.x)
+        {
+            this.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            this.transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
+    IEnumerator  AtkDown()
+    {
+        yield return new WaitForSeconds(0.5f);
+        LeftHand.transform.Translate(0, -0.3f, 0);
+        yield return new WaitForSeconds(1f);
+        LeftHand.transform.localPosition = new Vector3(0.14f, 1.58f, 0);
+    }
 }
